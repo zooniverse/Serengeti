1939ea21428cdcf2ae3f8fefa5858c6a696ca3d5
Stuart Lynn <stuart.lynn@gmail.com>
2012-12-06 11:07:12 -0600

    build script

---
+++ build.rb
@@ -0,0 +1,70 @@
+#!/usr/bin/env ruby
+
+require 'aws-sdk'
+AWS.config access_key_id: ENV['S3_ACCESS_ID'], secret_access_key: ENV['S3_SECRET_KEY']
+s3 = AWS::S3.new
+bucket = s3.buckets['www.snapshotserengeti.org']
+
+build = <<-BASH
+rm -rf build
+cp -R public pre_build_public
+cp -RL public build_public
+rm -rf public
+mv build_public public
+echo 'Building...'
+hem build
+mv public build
+mv pre_build_public public
+BASH
+
+timestamp = `date -u +%Y-%m-%d_%H-%M-%S`.chomp
+
+compress = <<-BASH
+echo 'Compressing...'
+
+timestamp=#{ timestamp }
+
+mv build/application.js "build/application-$timestamp.js"
+mv build/application.css "build/application-$timestamp.css"
+rm build/application.css
+
+BASH
+
+system build
+system compress
+
+index = File.read 'build/index.html'
+index.gsub! 'application.js', "application-#{ timestamp }.js"
+index.gsub! 'application.css', "application-#{ timestamp }.css"
+File.open('build/index.html', 'w'){ |f| f.puts index }
+
+working_directory = Dir.pwd
+Dir.chdir 'build'
+to_upload = Dir['**/*'].reject{ |path| File.directory? path }
+to_upload.delete 'index.html'
+to_upload << 'index.html'
+total = to_upload.length
+
+to_upload.each.with_index do |file, index|
+  content_type = case File.extname(file)
+  when '.html'
+    'text/html'
+  when '.js'
+    'application/javascript'
+  when '.css'
+    'text/css'
+  when '.gz'
+    'application/x-gzip'
+  when '.ico'
+    'image/x-ico'
+  else
+    `file --mime-type -b #{ file }`.chomp
+  end
+  
+  puts "#{ '%2d' % (index + 1) } / #{ '%2d' % total }: Uploading #{ file } as #{ content_type }"
+  bucket.objects["beta/#{file}"].write file: file, acl: :public_read, content_type: content_type
+end
+
+Dir.chdir working_directory
+`rm -rf build`
+puts 'Done!'
