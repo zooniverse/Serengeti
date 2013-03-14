67f714be4e0730f4233d3d0c17e950a1cabd076e
Brian Carstensen <brian.carstensen@gmail.com>
2013-03-14 09:44:35 -0500

    Reactivate deploy script

--- build.rb
+++ build.rb
@@ -26,7 +26,6 @@ timestamp=#{ timestamp }
 
 mv build/application.js "build/application-$timestamp.js"
 mv build/application.css "build/application-$timestamp.css"
-rm build/application.css
 
 BASH
 
@@ -38,33 +37,33 @@ index.gsub! 'application.js', "application-#{ timestamp }.js"
 index.gsub! 'application.css', "application-#{ timestamp }.css"
 File.open('build/index.html', 'w'){ |f| f.puts index }
 
-# working_directory = Dir.pwd
-# Dir.chdir 'build'
-# to_upload = Dir['**/*'].reject{ |path| File.directory? path }
-# to_upload.delete 'index.html'
-# to_upload << 'index.html'
-# total = to_upload.length
-# 
-# to_upload.each.with_index do |file, index|
-#   content_type = case File.extname(file)
-#   when '.html'
-#     'text/html'
-#   when '.js'
-#     'application/javascript'
-#   when '.css'
-#     'text/css'
-#   when '.gz'
-#     'application/x-gzip'
-#   when '.ico'
-#     'image/x-ico'
-#   else
-#     `file --mime-type -b #{ file }`.chomp
-#   end
-#   
-#   puts "#{ '%2d' % (index + 1) } / #{ '%2d' % total }: Uploading #{ file } as #{ content_type }"
-#   bucket.objects["#{file}"].write file: file, acl: :public_read, content_type: content_type
-# end
-# 
-# Dir.chdir working_directory
-# `rm -rf build`
-# puts 'Done!'
+working_directory = Dir.pwd
+Dir.chdir 'build'
+to_upload = Dir['**/*'].reject{ |path| File.directory? path }
+to_upload.delete 'index.html'
+to_upload << 'index.html'
+total = to_upload.length
+
+to_upload.each.with_index do |file, index|
+  content_type = case File.extname(file)
+  when '.html'
+    'text/html'
+  when '.js'
+    'application/javascript'
+  when '.css'
+    'text/css'
+  when '.gz'
+    'application/x-gzip'
+  when '.ico'
+    'image/x-ico'
+  else
+    `file --mime-type -b #{ file }`.chomp
+  end
+  
+  puts "#{ '%2d' % (index + 1) } / #{ '%2d' % total }: Uploading #{ file } as #{ content_type }"
+  bucket.objects["#{file}"].write file: file, acl: :public_read, content_type: content_type
+end
+
+Dir.chdir working_directory
+`rm -rf build`
+puts 'Done!'
