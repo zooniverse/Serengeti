45a696faa25a5d9137d206cfc36599b7ca8d0f36
Brian Carstensen <brian.carstensen@gmail.com>
2013-09-26 17:12:56 -0500

    Add camera trap sponsor (to satellite view, for now)

---
+++ public/sponsored_camera_traps.js
@@ -0,0 +1,15 @@
+window.SERENGETI_SPONSORED_CAMERA_TRAPS = {
+  'E06': 'Rafael P. Bedia',
+  'F05': 'McCrea family',
+  'F06': 'Turner Glynn',
+  'F12': 'El_Lion',
+  'G03': 'Libby Kerr',
+  'G04': 'Adam Wolkon',
+  'G05': 'Kristine and Bart',
+  'H05': 'the Anderson Family',
+  'H06': 'Kevin Boyd',
+  'S09': 'Lorrne Gates',
+  'I04': 'Bushtracks Expeditions (Bushtracks.com)',
+  'O06': 'Hannelore Schmidt',
+  'S09': 'Daniela F. Sieff'
+};
