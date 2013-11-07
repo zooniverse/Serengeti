9e9204e173898f239cfb327a92b915f0f682fbe3
Brian Carstensen <brian.carstensen@gmail.com>
2013-11-07 14:25:13 -0600

    Sort sponsors

--- public/sponsored_camera_traps.js
+++ public/sponsored_camera_traps.js
@@ -9,7 +9,7 @@ window.SERENGETI_SPONSORED_CAMERA_TRAPS = {
   'G05': 'Kristine and Bart',
   'H05': 'the Anderson Family',
   'H06': 'Kevin Boyd',
-  'S09': 'Lorrne Gates',
   'I04': 'Bushtracks Expeditions (Bushtracks.com)',
-  'O06': 'Hannelore Schmidt'
+  'O06': 'Hannelore Schmidt',
+  'S09': 'Lorrne Gates'
 };
